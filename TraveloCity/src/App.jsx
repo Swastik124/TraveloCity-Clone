92f42c60dbc3a56e3ec97db6a65d0e212a2b6ca8
Swastik124 <swastikmohanty124@gmail.com>
2024-07-21 11:58:40 +0530

    swastikChange

--- TraveloCity/src/App.jsx
+++ TraveloCity/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
 function App() {
 
   return (
     <>
-      
+      <div className='Header'>
+          <Header></Header>
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
