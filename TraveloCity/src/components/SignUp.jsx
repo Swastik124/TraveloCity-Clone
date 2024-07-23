f0b0e79fdbee3cf842c4784a8bcfbe1335d59877
Anindita-Panigrahi <aninditapanigrahi04@gmail.com>
2024-07-24 02:02:42 +0530

    Sign UP DONE

--- TraveloCity/src/components/SignUp.jsx
+++ TraveloCity/src/components/SignUp.jsx
@@ -1,71 +1,128 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react'
+import {toast} from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
 
-function Signup() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
+import { useState} from 'react';
+import './SignIn.css';
+import Home from './Home';
 
-  useEffect(() => {
-    axios.get('http://localhost:3000/userData')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        setError(error.message);
-      });
-  }, []);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (!firstName || !lastName || !email || !password) {
-      setError('Please fill out all fields');
-      return;
+export const SignUp = () => {
+    
+    const [userEmail, setUserEmail] = useState('');
+    const[userFirstName, setFirstName]= useState('');
+    const[userLastName,setLastName ] =useState('');
+    const [userpassword, setUserPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [keepSignedIn, setKeepSignedIn] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(ValidityState()){
+            console.log('Form Submitted');
+            fetch("http://localhost:3000/userData/").then((res)=>{
+                return res.json();
+            }).then((resp)=>{
+                console.log(resp);
+            }).catch((err)=>{
+                console.log(err);
+            })
+        }
     }
-    axios.post('https://example.com/api/users', {
-      firstName,
-      lastName,
-      email,
-      password,
-    })
-      .then(response => {
-        setData([...fetchedData, response.data]);
-        setFirstName('');
-        setLastName('');
-        setEmail('');
-        setPassword('');
-      })
-      .catch(error => {
-        setError(error.message);
-      });
-  };
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          First Name:
-          <input type="text" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
-        </label>
-        <label>
-          Last Name:
-          <input type="text" value={lastName} onChange={(event) => setLastName(event.target.value)} />
-        </label>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
-        </label>
-        <label>
-          Password:
-          <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
-        </label>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <button type="submit">Sign up</button>
-      </form>
-    </div>
-  );
+    const ValidityState = () => {
+        let result=true;
+        if(userEmail ==='' || userEmail=== null){
+            toast.warning('Please enter User email');
+        }
+        if(userFirstName==='' || userFirstName=== null){
+            toast.warning('Please enter First Name');
+        }
+        if(userLastName==='' || userLastName=== null){
+            toast.warning('Please enter Last Name');
+            }
+    
+    if(userpassword ==='' || userpassword=== null){
+        toast.warning('Please enter password');
+    }
+    return result;
 }
 
-export default Signup;
+    const handleCheckboxChange = () => {
+    setKeepSignedIn(!keepSignedIn);
+  };
+  function goTo(){
+    window.location.href = "/Home";
+  }
+
+    return (
+        
+        <div className="signInContainer">
+            
+            <div id="signInHead" style={{ height: '3rem' }}>
+                <button className='prevBtn' onClick={goTo}>
+                    <img src='https://www.svgrepo.com/show/356410/arrow-left-3.svg' />
+                </button>
+                <img id='headerLogo' style={{height:'1.875rem', padding:'0', margin:'0'}} src='https://www.travelocity.com/_dms/interstitial/logo.svg?locale=en_US&siteid=80001&2' />
+            </div><hr />
+    
+          <div className='signInBody'>
+            <h1>Create an account</h1>
+
+            <form onSubmit={handleSubmit}>
+            <label>
+                <input
+                  type="text"
+                  id="Email"
+                  placeholder='Email'
+                  value={userEmail} 
+                  onChange={e=>setUserEmail(e.target.value)}/>
+              </label>
+                <label>
+                    <input
+                    type="text"
+                    id='FirstName'
+                    placeholder='First Name'
+                    value={userFirstName} 
+                    onChange={e=>setFirstName(e.target.value)} />
+                </label>
+                <label>
+                    <input
+                    type="text"
+                    id='LastName'
+                    placeholder='Last Name'
+                    value={userLastName} 
+                    onChange={e=>setLastName(e.target.value)} />
+                </label>
+              
+              <label>
+                <input
+                  type="password"
+                  id="pass"
+                  placeholder='Password'
+                  value={userpassword} 
+                  onChange={e=>setUserPassword(e.target.value)}
+                 />
+              </label>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <input type="checkbox" checked={keepSignedIn} onChange={handleCheckboxChange} />
+                <label>Keep me signed in</label>
+              </div>
+              <p id="keepSignedInMessage" style={{ display: keepSignedIn ? 'block' : 'none' }}>Selecting this checkbox will keep you signed into your account on this device until you sign out. Do not select this on shared devices.</p><br/>
+              {error && <p style={{ color: 'red' }}>{error}</p>}
+    
+              <p>By signing in, I agree to the Travelocity <a href='https://www.travelocity.com/lp/lg-terms' style={{color:'blue', textDecoration:'underline'}}>Terms and Conditions </a>and <a href='https://www.travelocity.com/lp/b/lg-privacypolicy' style={{color:'blue', textDecoration:'underline'}}>Privacy Statement.</a></p><br/>
+    
+              <button id="submitBtn" type="submit">Continue</button>
+            </form>
+            
+          </div><br/>
+          <p>Already have an account <a href='SignIn'style={{color:'blue', textDecoration:'underline'}}>Sign In</a></p><br/>
+          <div className='icon'>
+            <img id='apple' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA5r0_FrSjm2OgttQLwh_CnVCnzbJ7dLv6oA&s'/>
+            <img id='fb' src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR3XyB5KnLk2ALgsLWUwpbVDuI6WOrmsdu9leOapY3J1YE8YJzd'/>
+          </div>
+    </div>
+  )
+}
+export default SignUp;
