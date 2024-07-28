ec4aafed08c782677f105004192ce1f25e0d223e
Akankshya02 <akankshyapattnaik5@gmail.com>
2024-07-29 01:06:09 +0530

    responsive destination details

--- TraveloCity/src/App.jsx
+++ TraveloCity/src/App.jsx
@@ -3,17 +3,18 @@ import Home from "./components/Home"
 //  import SignUp from './components/SignUp'
 // import SignIn from './components/SignIn';
 // import DestinationDetails from './components/DestinationDetails'
-// import Header from './components/Header'
-// import Footer from './components/Footer';
+import DestinationDetails from './components/DestinationDetails/DestinationDetails'
+import Header from './components/Header'
+import Footer from './components/Footer';
 import Navbar from './components/Navbar'
 
 function App() {
   return (
         <div>
-          {/* <div className="Header"><Header></Header></div> */}
+          <div className="Header"><Header></Header></div>
            <div className="Navbar"><Navbar></Navbar></div>
-          {/* <div className="Footer"><Footer></Footer></div> */}
-          {/* <DestinationDetails/> */}
+          <DestinationDetails/>
+          <div className="Footer"><Footer></Footer></div>
           {/*<FinalSubmit/>
             <Payment/>
             <PaymentForm/>
